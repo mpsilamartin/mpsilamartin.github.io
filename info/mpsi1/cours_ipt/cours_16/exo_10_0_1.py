b53fa672cbda5cbe8c8a217b3c8308c8d8eefab3
Matthieu Solnon <matthieu.solnon@gmail.com>
2019-03-28 15:47:03 +0100

    cdt matthieu

---
+++ info/mpsi1/cours_ipt/cours_16/exo_10_0_1.py
@@ -0,0 +1,13 @@
+from descente import resout
+
+from numpy import array
+
+A = array([[5.,8.,-2.],
+           [3.,1.,5.],
+           [0.,-2.,6.]])
+           
+B = array([[21.],
+           [16.],
+           [10.]])
+           
+X = resout(M,B)
