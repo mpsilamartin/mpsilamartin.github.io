2609dfc36bc3db7612120a88dc07142b683f6ece
mlongmaths <maths@mlong.fr>
2020-01-31 17:26:30 +0100

    Update script_install_part1.sh

--- info/TP_opt/script_install_part1.sh
+++ info/TP_opt/script_install_part1.sh
@@ -1,2 +1,6 @@
 apt install --yes emacs tuareg-mode opam
-curl -sL https://raw.githubusercontent.com/ocaml/opam/master/shell/install.sh | sh
+TMP=${TMPDIR:-/tmp}
+OPAM_INSTALL="opam_install-${TAG}-${ARCH}-${OS}"
+curl -sL https://raw.githubusercontent.com/ocaml/opam/master/shell/install.sh > $TMP/$OPAM_INSTALL
+bash $TMP/$OPAM_INSTALL
+rm $TMP/$OPAM_INSTALL
