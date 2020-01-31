3c9d9a5fdd35cb0ae986533b240a704d1145c93e
mlongmaths <maths@mlong.fr>
2020-01-31 16:56:56 +0100

    Update script_install_part1.sh

--- info/TP_opt/script_install_part1.sh
+++ info/TP_opt/script_install_part1.sh
@@ -1,2 +1,2 @@
 apt install --yes emacs tuareg-mode opam
-sh <(curl -sL https://raw.githubusercontent.com/ocaml/opam/master/shell/install.sh)
+curl -sL https://raw.githubusercontent.com/ocaml/opam/master/shell/install.sh | sh
