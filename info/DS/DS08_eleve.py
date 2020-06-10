469de65fadbc7e215b563524aea94b638480cd5d
emiliendurif <emilien.durif@gmail.com>
2020-06-10 16:09:24 +0200

    maj d08S

---
+++ info/DS/DS08_eleve.py
@@ -0,0 +1,59 @@
+NOM = ""
+Prenom = ""
+Classe = ""
+alpha=""
+
+## Question 1
+Q1_req = ""
+
+## Question 2
+Q2_req = ""
+Q2_res = ""
+
+## Question 3
+Q3_req = ""
+
+## Question 4
+Q4_req = ""
+
+
+## Question 5
+Q5_req = ""
+Q5_res = ""
+
+## Question 6
+Q6_req = ""
+
+
+## Question 7
+Q7_req = ""
+
+## Question 8
+Q8_req = ""
+
+## Question 9
+Q9_req = ""
+Q9_res = ""
+
+## Question 10
+Q10_req = ""
+Q10_res = ""
+
+## Question 11
+Q11_req = ""
+Q11_res = ""
+
+## Question 12
+Q12_req = ""
+
+
+## Question 13
+Q13_req = ""
+
+## Question 14
+Q14_req = ""
+
+
+## Question 15
+Q15_req = ""
+Q15_res ="nom,prenom,montant"
