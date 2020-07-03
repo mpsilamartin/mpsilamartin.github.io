4e542fe33a06457da847e86e5eeb694b60eb5c5d
Skander <szannad@lamartin.fr>
2020-07-04 01:12:47 +0200

    tests php

---
+++ entetes/orga1.php
@@ -0,0 +1,36 @@
+<script type="text/javascript">
+      $(document).ready(function(){
+      $('#target-onglet-actif-orga').load('https://mpsilamartin.github.io/menus/onglet-actif-orga.html');
+      });
+    </script>  
+        
+    <script type="text/javascript">
+      $(document).ready(function(){
+      $('#target-onglet-maths').load('https://mpsilamartin.github.io/menus/onglet-maths.html');
+      });
+    </script>  
+        
+    <script type="text/javascript">
+      $(document).ready(function(){
+      $('#target-onglet-info').load('https://mpsilamartin.github.io/menus/onglet-info.html');
+      });
+    </script> 
+     	
+    <script type="text/javascript">
+      $(document).ready(function(){
+      $('#target-onglet-physique').load('https://mpsilamartin.github.io/menus/onglet-physique.html');
+      });
+    </script> 
+    
+    <script type="text/javascript">
+      $(document).ready(function(){
+      $('#target-onglet-sii').load('https://mpsilamartin.github.io/menus/onglet-sii.html');
+      });
+    </script> 
+    
+    <script type="text/javascript">
+      $(document).ready(function(){
+      $('#target-onglet-tipe').load('https://mpsilamartin.github.io/menus/onglet-tipe.html');
+      });
+    </script> 
+    
