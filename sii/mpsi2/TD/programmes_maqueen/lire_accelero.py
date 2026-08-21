5e50123c8bc80412a49d573ed3e93c7fdcc62f68
emiliendurif <emilien.durif@gmail.com>
2026-08-21 16:19:03 +0200

    maj sii rentree

--- sii/mpsi2/TD/programmes_maqueen/lire_accelero.py
+++
@@ -1,8 +0,0 @@
-from microbit import *
-
-while True:
-    #  Lire des données de l'accelerometer
-    acc = accelerometer.get_values()
-    #  Affichage des donnees
-    print(acc)
-    sleep(50)  # Pause entre chaque mesure
